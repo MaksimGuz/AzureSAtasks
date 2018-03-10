4c8afe46de20c13c7d87de5346bcb39dc6b99b56
Maksim <maksim.guz@gmail.com>
2018-03-10 17:21:51 +0300

    add solution

---
+++ task01_FaceApiMvcWebApp/FaceApiMvcWebApp/Infrastructure/ConnectionString.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+
+namespace FaceApiMvcWebApp.Infrastructure
+{
+    public static class ConnectionString
+    {
+        //static string account = CloudConfigurationManager.GetSetting("StorageAccountName");
+        //static string key = CloudConfigurationManager.GetSetting("StorageAccountKey");
+        static string contianer = CloudConfigurationManager.GetSetting("StorageAccountBlobContainerName");
+        public static CloudStorageAccount GetConnectionString()
+        {
+            //string connectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", account, key);
+            return CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+        }
+        public static string GetCloudStorageBlobContainerName()
+        {
+            return contianer;
+        }
+    }
+}
